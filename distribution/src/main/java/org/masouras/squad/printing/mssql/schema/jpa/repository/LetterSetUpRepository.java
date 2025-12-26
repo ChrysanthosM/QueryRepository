2abc42e18bb60cb44f848af6008b899a5377e786
ChrysanthosM <ChrysanthosM@Outlook.com>
2025-12-26 16:06:27 +0200

    commit

--- distribution/src/main/java/org/masouras/squad/printing/mssql/schema/jpa/repository/LetterSetUpRepository.java
+++ distribution/src/main/java/org/masouras/squad/printing/mssql/schema/jpa/repository/LetterSetUpRepository.java
@@ -1,6 +1,7 @@
 package org.masouras.squad.printing.mssql.schema.jpa.repository;
 
 import org.masouras.squad.printing.mssql.schema.jpa.entity.LetterSetUpEntity;
+import org.masouras.squad.printing.mssql.schema.jpa.entity.LetterSetUpKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LetterSetUpRepository extends JpaRepository<LetterSetUpEntity, Long> {}
+public interface LetterSetUpRepository extends JpaRepository<LetterSetUpEntity, LetterSetUpKey> {}
--- distribution/src/main/java/org/masouras/squad/printing/mssql/schema/jpa/repository/PrintingSetUpRepository.java
+++ distribution/src/main/java/org/masouras/squad/printing/mssql/schema/jpa/repository/PrintingSetUpRepository.java
@@ -1,6 +1,7 @@
 package org.masouras.squad.printing.mssql.schema.jpa.repository;
 
 import org.masouras.squad.printing.mssql.schema.jpa.entity.PrintingSetUpEntity;
+import org.masouras.squad.printing.mssql.schema.jpa.entity.PrintingSetUpKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PrintingSetUpRepository extends JpaRepository<PrintingSetUpEntity, Long> {}
+public interface PrintingSetUpRepository extends JpaRepository<PrintingSetUpEntity, PrintingSetUpKey> {}
